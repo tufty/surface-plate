a16c63407189a965b1342c1324772d1c10e12942
Simon Stapleton <simon.stapleton@gmail.com>
2021-04-02 17:13:12 +0200

    remove unused crap

--- js/spherometer/app.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', function () {
-  // Initialise everything
-  init_2d();
-  init_3d();
-
-  // Trigger an initial decomposition of the surface
-  document.getElementById('triangulate-button').click();
-})
